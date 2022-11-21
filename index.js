2d2f6849e09f3ad6fd7e8786c6ff6fe112e3f089
Hirangi5 <102467513+Hirangi5@users.noreply.github.com>
2022-11-22 02:46:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+// console.log(num1);
+const questionEl = document.getElementById("question");
+questionEl.innerHTML = `What is ${num1} multiplied by ${num2}?`
+
+const correctAns = num1*num2;
+const inputE1 = document.getElementById("input");
+
+let score = 0;
+score = JSON.parse(localStorage.getItem("score"));
+
+const formE1 = document.getElementById("form");
+formE1.addEventListener("submit", ()=>{
+const userAns = +inputE1.value 
+if(userAns ===correctAns){
+ score++;
+ updateLocalStorage()
+}
+
+else{
+    score--;
+    updateLocalStorage()
+}
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
+// console.log(score)
+
+const scoreE1 = document.getElementById("score");
+scoreE1.innerText = `score:${score}`;
